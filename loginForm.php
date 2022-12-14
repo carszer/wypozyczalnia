f2ec117b00795c09fce3c480e45c94bcccc7e63c
rafalkaminski111 <legustogaming@gmail.com>
2022-12-14 13:10:39 +0100

    mockup wyglądu

--- loginForm.php
+++ loginForm.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl-PL">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -20,10 +20,10 @@
             <div class="collapse navbar-collapse" id="navbarCollapse">
               <ul class="navbar-nav m-auto mb-2 justify-content-center mb-md-0">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Strona główna</a>
+                  <a class="nav-link active" aria-current="page" href="index.html">Strona główna</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Oferta</a>
+                  <a class="nav-link" href="oferta.html">Oferta</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Rezerwuj</a>
@@ -35,16 +35,90 @@
                   <a class="nav-link" href="#">O nas</a>
                 </li>  
               </ul>
-              <div class="text-end">
-                <button type="button" class="btn btn-outline-light me-2">Zaloguj się</button>
-                <button type="button" class="btn btn-warning">Zarejestruj się</button>
+              < <a class="btn btn-outline-light me-2" href="loginForm.php">Zaloguj się</a>
+                 <!--<a class="btn btn-warning me-2" href="logowanie.html">Zaloguj się</a> -->
+
+                  <a class="btn btn-warning" href="registerForm.php">Zarejestruj się</a>
+                  <!--<a class="btn btn-outline-light me-2" href="registerForm.html">Zarejestruj się</a>-->
               </div>
             </div>
           </div>
         </nav>
       </header>
-    
-    
 
+      <main>  
+        <div class="position-relative overflow-hidden p-3 p-md-5  text-center bg-light">
+          <div class="col-md-5 p-lg-5 mx-auto my-5 "> 
+            <form>
+              <img class="mb-4" src="img/small-logo.png" alt="" width="150" height="100">
+              <h1 class="h1 mb-3 fw-normal m-md-3">Zaloguj się</h1>
+          
+              <div class="form-floating m-md-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Adres E-mail</label>
+              </div>
+              <div class="form-floating m-md-3">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Hasło</label>
+                <small id="registerHelp" class="">Nie masz konta? Zarejestruj się klikajac <a href="registerForm.html">Tutaj</a></small>
+              </div>
+
+            </br>
+            </br>
+              <button class="w-50 btn btn-lg btn-primary" type="submit">Zaloguj się</button>
+              <p class="mt-5 mb-3 text-muted">&copy; 2022–2022</p>
+            </form>
+          </div>
+          <div class="product-device shadow-sm d-none d-md-block"></div>
+          <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+        </div>
+      </main>
+
+      <footer class="container py-5">
+        <div class="row">
+          <div class="col-12 col-md">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
+            <small class="d-block mb-3 text-muted">&copy; 2017–2022</small>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Features</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="link-secondary" href="#">Cool stuff</a></li>
+              <li><a class="link-secondary" href="#">Random feature</a></li>
+              <li><a class="link-secondary" href="#">Team feature</a></li>
+              <li><a class="link-secondary" href="#">Stuff for developers</a></li>
+              <li><a class="link-secondary" href="#">Another one</a></li>
+              <li><a class="link-secondary" href="#">Last time</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Resources</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="link-secondary" href="#">Resource name</a></li>
+              <li><a class="link-secondary" href="#">Resource</a></li>
+              <li><a class="link-secondary" href="#">Another resource</a></li>
+              <li><a class="link-secondary" href="#">Final resource</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Resources</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="link-secondary" href="#">Business</a></li>
+              <li><a class="link-secondary" href="#">Education</a></li>
+              <li><a class="link-secondary" href="#">Government</a></li>
+              <li><a class="link-secondary" href="#">Gaming</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>About</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="link-secondary" href="#">Team</a></li>
+              <li><a class="link-secondary" href="#">Locations</a></li>
+              <li><a class="link-secondary" href="#">Privacy</a></li>
+              <li><a class="link-secondary" href="#">Terms</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
 </body>
 </html>
