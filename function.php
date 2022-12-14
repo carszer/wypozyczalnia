70a584a077010e5fad151dbfcfbf345ce59b4bb3
rafalkaminski111 <legustogaming@gmail.com>
2022-12-14 14:28:00 +0100

    fix

--- function.php
+++ function.php
@@ -1,8 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "", "portal");
-    if (mysqli_connect_errno()) {
-        echo "Connnection failed: " . mysqli_connect_error();
-    }
+
     function test_input($data) {
       $data = trim($data);
       $data = stripslashes($data);
@@ -11,17 +8,21 @@
     }
 
     function log(){
+        $conn = mysqli_connect("localhost","root","","portal");
+        if(mysqli_connect_errno()) { 
+            echo "Connnection failed: ".mysqli_connect_error();
+        }
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             $email = test_input($_POST["email"]);
             $password = test_input($_POST["password"]);
         }
-        if (mysqli_query($conn, 'SELECT haslo from uzytkownicy where login = "' . $login . '";')) {
-            $q3 = mysqli_query($conn, 'SELECT haslo from uzytkownicy where login = "' . $login . '";');
+        if (mysqli_query($conn, 'SELECT haslo from uzytkownicy where email = "' . $email . '";')) {
+            $q3 = mysqli_query($conn, 'SELECT haslo from uzytkownicy where email = "' . $email . '";');
             if (mysqli_num_rows($q3) > 0) {
                 $row = mysqli_fetch_assoc($q3);
                 $haslo = $row['haslo'];
-                $hhaslo = sha1($fhaslo);
+                $hhaslo = sha1($password);
                 if ($haslo == $hhaslo) {
 
                 } else {
