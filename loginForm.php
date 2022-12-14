febdb1f09fcde26bd21883faece1593a956dd0ef
rafalkaminski111 <legustogaming@gmail.com>
2022-12-14 01:02:49 +0100

    start form

---
+++ loginForm.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>CarSzer</title>
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <header>
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">CarSzer</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+              <ul class="navbar-nav m-auto mb-2 justify-content-center mb-md-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Strona główna</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Oferta</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Rezerwuj</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Kontakt</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">O nas</a>
+                </li>  
+              </ul>
+              <div class="text-end">
+                <button type="button" class="btn btn-outline-light me-2">Zaloguj się</button>
+                <button type="button" class="btn btn-warning">Zarejestruj się</button>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+    
+    
+
+</body>
+</html>
