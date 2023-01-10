8967a59fba4de208dd72cc8580aee6f86f799af9
Pablo <23451@edu.amw.gdynia.pl>
2023-01-10 22:44:50 +0100

    ok

--- footer.php
+++ footer.php
@@ -1,45 +1,69 @@
 <html>
-<link rel="stylesheet" href="footer.css">
-<footer>
-      <section>
-        <div>
-          <div id="row">
-            <div id="column">
-              <h6>CarSzer</h6>
-              <p>
-                Jesteśmy prężnie działającą firmą wynajmujące samochody marek luksusowych.
-                <br>
-                CarSzer, z nami stać Cię na wynajem!
-                Tu jeszcze logo wleci.
-              </p>
+  <body>
+    <footer
+            class="text-center text-lg-start text-white"
+            style="background-color: #1c2331"
+            >
+      <section class="">
+        <div class="container text-center text-md-start mt-5">
+          <div class="row mt-3">
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <h6 class="text-uppercase fw-bold">CarSher</h6>
+                <hr
+                    class="mb-4 mt-0 d-inline-block mx-auto"
+                   style="width: 80px; background-color: #9b1a1a; height: 3px"
+                  />
+                  <p>
+                    Jesteśmy świeżą wschodzącą marką na polskim rynku aut prawie luksusowych.
+                    CarSher, z nami stać Cię na wynajem! 
+                   </p>
             </div>
-            <div id="column">
-              <h6>Przydatne linki</h6>
-              <p>
-                <a href="#!" >O nas</a>
-              </p>
-              <p>
-                <a href="#!" >Wynajem aut - ważne</a>
-              </p>
-              <p>
-                <a href="#!" >Klauzula informacyjna</a>
-              </p>
-              <p>
-                <a href="#!" >Polityka prywatności</a>
-              </p>
+  
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+              <h6 class="text-uppercase fw-bold">Przydatne linki</h6>
+                <hr
+                 class="mb-4 mt-0 d-inline-block mx-auto"
+                  style="width: 80px; background-color: #9b1a1a; height: 3px"
+                />
+                <p>
+                  <a href="#!" class="text-white">O nas</a>
+                </p>
+                <p>
+                  <a href="#!" class="text-white">Promocje</a>
+                </p>
+                <p>
+                  <a href="#!" class="text-white">Wynajem aut - ważne</a>
+                </p>
+                <p>
+                  <a href="#!" class="text-white">Klauzula informacyjna</a>
+                </p>
+                <p>
+                  <a href="#!" class="text-white">Polityka prywatności</a>
+                </p>
             </div>
-            <div id="column">
-              <h6>Kontakt</h6>
-              <p>534 545 432</p>
-              <p><a href="mailto:carszer@gmail.com">carszer@gmail.com</a></p>
-              <p>Śmidowicza 69, 81-127 Gdynia</p>
+
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <h6 class="text-uppercase fw-bold">Kontakt</h6>
+                <hr
+                  class="mb-4 mt-0 d-inline-block mx-auto"
+                  style="width: 80px; background-color: #9b1a1a; height: 3px"
+                />
+                <p><i class="fas fa-home mr-3"></i> Siedziba: Gdynia, Akademia Małych Wymagań, PL</p>
+                <p><i class="fas fa-envelope mr-3"></i> E-mail: CarSher@gmail.com</p>
+                <p><i class="fas fa-phone mr-3"></i> Tel: + 48 555-111-222</p>
             </div>
           </div>
         </div>
-      </section>
-      <div id="copyright">
-        <b>CARSZER 2022 - 2023</b> © Wszystkie prawa zastrzeżone
+        </section>
+         <div
+           class="text-center p-3"
+           style="background-color: rgba(0, 0, 0, 0.2)"
+           >
+           © 2022-2023 Carszer
+           <a class="text-white" href="https://CarSzer.com/"
+           >Strona główna</a
+          >
       </div>
     </footer>
-</body>
+  </body>
 </html>
