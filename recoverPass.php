1657a064fe8f526be8bfd0793629c22a5e5f1a1d
rafalkaminski111 <legustogaming@gmail.com>
2023-01-10 23:47:59 +0100

    captcha

--- recoverPass.php
+++ recoverPass.php
@@ -9,6 +9,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>CarSzer</title>
+    <style>
+    .g-recaptcha{
+          width: min-content;
+        }
+    </style>
 </head>
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
@@ -62,10 +67,14 @@
               <div class="form-floating m-md-3">
                 <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="mail">
                 <label for="floatingInput">Adres E-mail</label>
-              </br>
-                </br>
-                <button class="w-50 btn btn-lg btn-primary" type="submit" name="send_btn">Wyślij kod odzyskiwania</button>
+              </div>
+              <div class="form-floating m-md-3">
+              <div class="mx-auto g-recaptcha" data-sitekey="6LfWsukjAAAAAC2hSiSZOJsf3UeZFMOfmPu21Kae" data-callback='onSubmit' data-action='submit'></div>
+              </div>
+              <button class="w-50 btn btn-lg btn-primary" type="submit" name="send_btn">Wyślij kod odzyskiwania</button>
               <p class="mt-5 mb-3 text-muted">&copy; 2022–2022</p>
+            </form>
+            
             <?php
               if(isset($_SESSION['error']))
               { 
