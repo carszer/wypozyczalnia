dc288fa4c48ab2972bc9735d9774f1a1303b0561
rkaminski111 <23437@edu.amw.gdynia.pl>
2023-06-01 21:59:02 +0200

    CAR-152

--- deleteCar.php
+++ deleteCar.php
@@ -4,6 +4,7 @@ if (isset($_SESSION['admin'])) {
     header('Location:usuwanieAut.php');
 }
 ?>
+
 <?php
 require_once "connect.php";
 $idcar = $_POST['car'];
@@ -11,12 +12,17 @@ $connect = new mysqli($host, $db_user, $db_pass, $db_name);
 
 if (isset($_POST['car'])) {
     $idcar = $_POST['car'];
-    $rezIstnieje = $connect->query("SELECT reservation.idcar FROM reservation INNER JOIN car ON reservation.idcar = $idcar'");
+    $rezIstnieje = $connect->query("SELECT reservation.idcar FROM reservation INNER JOIN car ON reservation.idcar = $idcar");
     $rezerwacja = $rezIstnieje->num_rows;
-    if ($rezerwacja >= 0) {
+    if ($rezerwacja > 0) {
         $sql1 = "UPDATE car SET visible = 0 WHERE idcar = $idcar";
         $connect->query($sql1);
-        header('Location:usuwanieAut.php');
     }
+     else {
+        $sql = "DELETE FROM car WHERE idcar = $idcar";
+        $connect->query($sql);
+    }
+    $connect->close();
+    header('Location:usuwanieAut.php');
 }
 ?>
